b32cf877343fa0f2565fa80683c8b96b151876a5
Robert Grembowicz <robert.grembowicz@gmail.com>
2021-07-09 18:53:46 +0200

    configure styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
-  return <div>test</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>test</div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+import { css } from 'styled-components';
+
+const colors = css`
+  :root {
+    --color-white: #fff;
+    --color-white-rgb: 255, 255, 255;
+    --color-black: #000;
+    --color-black-rgb: 0, 0, 0;
+    --color-primary: #096ce9;
+    --col-primary-rgb: 09, 108, 233;
+  }
+`;
+
+export default colors;
